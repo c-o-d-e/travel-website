76d4455cabb5cfb3f3a7ba689efc7da28405107d
c-o-d-e <swapyuk@gmail.com>
2022-09-24 21:45:56 +0100

    boilerplate for other pages

---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../App.css";
+
+function Services() {
+    return (
+        <div>
+            <h1>Something</h1>
+        </div>
+    );
+}
+
+export default Services;
