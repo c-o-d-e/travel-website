eb07b807c2aeb017fc4b2782f6ab951dddbf208c
c-o-d-e <swapyuk@gmail.com>
2022-09-24 15:02:37 +0100

    replaced icon classname and brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        TRVL <i className="fab fa-typo3"></i>
+                        TRAVELIO <i className="fa-solid fa-plane-departure"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? "fas fa-times" : "fas fa-bars"} />
