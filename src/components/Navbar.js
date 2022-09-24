f1a402d75c0a14325c58b3a801f976a53ea19e5a
c-o-d-e <swapyuk@gmail.com>
2022-09-24 13:53:23 +0100

    Link (a tag) added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <>
-            <h1>This is Navbar.js</h1>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TRVL <i className="fab fa-typo3"></i>
+                    </Link>
+                </div>
+            </nav>
         </>
     );
 }
