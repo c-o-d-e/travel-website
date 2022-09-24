4cbe7774efc2c39c19e578cdd722a10db3322256
c-o-d-e <swapyuk@gmail.com>
2022-09-24 15:10:42 +0100

    close mobile menu on clicking the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import "./Navbar.css";
@@ -23,13 +23,21 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener("resize", showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link
+                        to="/"
+                        className="navbar-logo"
+                        onClick={closeMobileMenu}
+                    >
                         TRAVELIO <i className="fa-solid fa-plane-departure"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
