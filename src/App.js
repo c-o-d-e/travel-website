debab2c151c5a8aa5d459354b4de9bf75d2993e5
c-o-d-e <swapyuk@gmail.com>
2022-09-25 17:29:12 +0100

    travel-website added to basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <>
             <Navbar />
             <Routes>
-                <Route path="/" exact element={<Home />} />
+                <Route path="/travel-website" exact element={<Home />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/sign-up" element={<SignUp />} />
