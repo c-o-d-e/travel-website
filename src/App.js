1f2046ae60d1b055e0745337dfa18725e05351ca
c-o-d-e <swapyuk@gmail.com>
2022-09-25 16:47:56 +0100

    tryinh hashrouter in diff location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
@@ -10,15 +10,15 @@ import SignUp from "./components/pages/SignUp";
 function App() {
     return (
         <>
-            <Router>
-                <Navbar />
+            <Navbar />
+            <HashRouter>
                 <Routes>
                     <Route path="/" exact element={<Home />} />
                     <Route path="/services" exact element={<Services />} />
                     <Route path="/products" exact element={<Products />} />
                     <Route path="/sign-up" exact element={<SignUp />} />
                 </Routes>
-            </Router>
+            </HashRouter>
         </>
     );
 }
