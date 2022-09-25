b08ffe0f705c2648a23f35c80d606932299b0b7e
c-o-d-e <swapyuk@gmail.com>
2022-09-25 17:22:46 +0100

    exact path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <>
             <Navbar />
             <Routes>
-                <Route path="/travel-website/" element={<Home />} />
+                <Route path="/travel-website" exact element={<Home />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/sign-up" element={<SignUp />} />
