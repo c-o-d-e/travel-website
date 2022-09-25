fa958b4bbee81ce00fdab47b1b05d23c03ab4804
c-o-d-e <swapyuk@gmail.com>
2022-09-25 17:29:26 +0100

    travel-website added to basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <>
-            <BrowserRouter Basename={process.env.PUBLIC_URL}>
+            <BrowserRouter Basename="/travel-website">
                 <App />
             </BrowserRouter>
         </>
